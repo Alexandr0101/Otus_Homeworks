8a7c26a229d87b9a33d0f82b079b9639bad2969c
Alex_mba <damirpugh@gmail.com>
2026-07-15 01:02:13 +0300

    Add new menu point and two functions

--- PhoneBook/functions.py
+++ PhoneBook/functions.py
@@ -1,10 +1,39 @@
-def open_file():
+def open_file() -> list[str]:
+    """open file or create and open file"""
     with open('/Users/alex/OTUS/Homeworks/PhoneBook/Contacts.txt', 'a+') as f:
         f.seek(0)
         return f.readlines()
 
-def open_and_save():
-   contact_data = open_file()
-   with open('/Users/alex/OTUS/Homeworks/PhoneBook/Contacts.txt', 'w') as f:
-       f.writelines(contact_data)
-       return 'Success'
+def open_and_save() -> str:
+    """open and rewrite existing file"""
+    contact_data = open_file()
+    with open('/Users/alex/OTUS/Homeworks/PhoneBook/Contacts.txt', 'w') as f:
+        f.writelines(contact_data)
+        return 'Success'
+
+def is_contact_in_contacts(last_name : str, first_name : str) -> bool:
+    with open('/Users/alex/OTUS/Homeworks/PhoneBook/Contacts.txt', 'r') as f:
+        for i in f:
+            if last_name in i and first_name in i:
+                return True
+    return False
+
+def check_input(last_name : str = '', first_name: str = '', phone_num: str = '') -> bool:
+    if last_name:
+        if last_name.isalpha():
+            return True
+    if first_name:
+        if first_name.isalpha():
+            return True
+    if phone_num:
+        if phone_num.isdigit():
+            return True
+    return False
+
+def add_contact(last_name: str, first_name: str, phone_num: str) -> str:
+    file = open_file()
+    file.append(f'{last_name} {first_name} {phone_num};\n')
+    with open('/Users/alex/OTUS/Homeworks/PhoneBook/Contacts.txt', 'w') as f:
+        f.writelines(file)
+
+
--- PhoneBook/main.py
+++ PhoneBook/main.py
@@ -1,4 +1,4 @@
-from functions import open_file, open_and_save
+from functions import open_file, open_and_save, check_input, is_contact_in_contacts, add_contact
 
 while True:
     response_num = int(input("""Привет:) Добро пожаловать в телефонный справочник!
@@ -15,9 +15,37 @@ while True:
         print('Пока')
         break
     elif response_num == 1:
-        pass
+        print(open_file())
     elif response_num == 2:
-        pass
+        while True:
+            last_name = input('Введи фамилию: \n'
+                              'Для выхода введи 1 \n')
+            if last_name == '1':
+                break
+            check_last_name =  check_input(last_name = last_name)
+            if check_last_name:
+                first_name = input('Введи имя: \n'
+                                   'Для выхода введи 1 \n')
+                if first_name == '1':
+                    break
+                check_firstname = check_input(first_name = first_name)
+                if check_firstname:
+                    contact_data = is_contact_in_contacts(last_name, first_name)
+                    if not contact_data:
+                        phone_num = input('Введи номер телефона: ')
+                        check_phone_num = check_input(phone_num = phone_num)
+                        if check_phone_num:
+                            contact_data = add_contact(last_name, first_name, phone_num)
+                            print(f'Контакт {last_name} {first_name} успешно добавлен')
+                            break
+                        else:
+                            print('Вводите только цифры: ')
+                    else:
+                        print(f'Контакт {last_name} {first_name} уже существует')
+                else:
+                    print('Вводите только буквы: ')
+            else:
+                print('Вводите только буквы: ')
     elif response_num == 3:
         pass
     elif response_num == 4:
